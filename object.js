f3e7a258d138520902e43fe5f925cb48237abda2
phytertek <ryan.phytertek@gmail.com>
2018-05-05 22:00:12 -0400

    publish patch

--- object.js
+++ object.js
@@ -1,11 +1,13 @@
-const has = k => o => o.hasOwnProperty(k);
-const keys = o => oect.keys(o);
-const vals = o => oect.values(o);
-const entries = o => oect.entries(o);
-const assign = k => v => o => ({ ...o, [k]: v });
-const remove = k => o =>
+const { curry } = require('./core');
+
+const has = (k, o) => o.hasOwnProperty(k);
+const keys = o => Object.keys(o);
+const vals = o => Object.values(o);
+const entries = o => Object.entries(o);
+const assign = (k, v, o) => ({ ...o, [k]: v });
+const remove = (k, o) =>
   keys(o).reduce((r, ok) => (k !== ok ? { ...r, [ok]: o[ok] } : r), {});
-const evolve = m => o =>
+const evolve = (m, o) =>
   keys(m).reduce((r, k) => (has(k)(o) ? { ...r, [k]: m[k](o[k]) } : r), o);
 const getFirstKey = o => keys(o)[0];
 const getLastKey = o => keys(o).slice(-1)[0];
@@ -14,14 +16,11 @@ const getLastVal = o => o[getLastKey(o)];
 const dropFirstKey = o => remove(getFirstKey(o))(o);
 const dropLastKey = o => remove(getLastKey(o))(o);
 
-module.exports = {
-  has,
+const nAry = { has, assign, remove, evolve };
+const unary = {
   keys,
   vals,
   entries,
-  assign,
-  remove,
-  evolve,
   getFirstKey,
   getLastKey,
   getFirstVal,
@@ -29,3 +28,8 @@ module.exports = {
   dropFirstKey,
   dropLastKey
 };
+
+const curryAll = o =>
+  keys(o).reduce((r, k) => ({ ...r, [k]: curry(o[k]) }), {});
+
+module.exports = { ...unary, ...curryAll(nAry) };
